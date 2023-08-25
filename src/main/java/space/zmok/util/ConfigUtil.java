0fdd265dc9068bd72d1d214b1f83ce8d870a3e3e
solcesby <solcesby@gmail.com>
2023-08-25 06:19:10 +0300

    Create ConfigUtil

---
+++ src/main/java/space/zmok/util/ConfigUtil.java
@@ -0,0 +1,22 @@
+package space.zmok.util;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+import java.util.Map;
+
+public class ConfigUtil {
+
+    public static Map<String, String> VARIABLES;
+
+    {
+        Yaml yaml = new Yaml();
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("application.yml");
+        VARIABLES = yaml.load(inputStream);
+    }
+
+    static {
+        new ConfigUtil();
+    }
+
+}
