f5329e67c24e5798f6e95296daeb3bed3d2aa6d5
solcesby <solcesby@gmail.com>
2023-09-04 05:24:18 +0300

    Create and implement Transaction Repository

---
+++ src/main/java/space/zmok/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package space.zmok.repository;
+
+import space.zmok.entity.transaction.TransactionEntity;
+
+import java.util.UUID;
+
+public interface TransactionRepository extends BaseRepository<TransactionEntity, UUID> {
+
+}
